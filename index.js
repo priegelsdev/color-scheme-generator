870e8958ae805c634fedf6804759f52aae577697
priegelsdev <priegelsdev@gmail.com>
2023-01-08 18:49:27 +0100

    write basic functions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
    */
 
 
-fetch('https://www.thecolorapi.com/id?hex=24B1E0')
+/* fetch('https://www.thecolorapi.com/id?hex=24B1E0')
     .then(res => res.json())
     .then(data => {
         const color = data
@@ -16,7 +16,48 @@ fetch('https://www.thecolorapi.com/id?hex=24B1E0')
         document.querySelector('.color-pick').style.backgroundColor = `#${color.hex.clean}`
     }) 
 
+    console.log(document.querySelector('.color-pick')) */
 
-    console.log(document.querySelector('.color-pick'))
+// initialization of variables to be used
+
+let hexValue;
+
+  document.addEventListener('click', function(e) {
+    if (e.target.tagName === 'BUTTON') {
+      renderColors()
+    }
+
+  })
+
+// function to retrieve hex code from input and save to variable for later param use
+    
+function getHex() {
+  hexValue = document.querySelector('input').value.substring(1);
+  
+  return hexValue
+}
+
+// function to send color pick to API and retrieve color scheme
+
+function getColorScheme(hex) {
+  getHex();
+
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hex}`, {
+    method: 'GET'
+  })
+    .then(res => res.json())
+    .then(data => { console.log(data)
+      
+    })
+}
+
+// function to render retrieved color scheme 
+
+function renderColors() {
+  getColorScheme(hexValue);
+}
+
+
 
 
